d820a86df7d04ebdcec6c25f722c281c75dbb2e3
Taneros <fricentaurus@GMAIL.COM>
2023-01-10 10:18:20 +0300

    fix: styles

--- src/components/Cart/CheckOut.jsx
+++ src/components/Cart/CheckOut.jsx
@@ -52,34 +52,37 @@ const Checkout = ({onConfirm, onCancel}) => {
   };
 
   return (
-    <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={`${classes.control} ${fromInputsValidity.name ? '' : classes.invalid}`}>
-        <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" ref={nameInputRef} />
-        {!fromInputsValidity.name && <p>Name is not valid!</p>}
-      </div>
-      <div className={`${classes.control} ${fromInputsValidity.street ? '' : classes.invalid}`}>
-        <label htmlFor="street">Street</label>
-        <input type="text" id="street" ref={streetInputRef} />
-        {!fromInputsValidity.street && <p>Street name is not valid!</p>}
-      </div>
-      <div className={`${classes.control} ${fromInputsValidity.postalCode ? '' : classes.invalid}`}>
-        <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" ref={postalInputRef} />
-        {!fromInputsValidity.postalCode && <p>Postal Code is not valid!</p>}
-      </div>
-      <div className={`${classes.control} ${fromInputsValidity.city ? '' : classes.invalid}`}>
-        <label htmlFor="city">City</label>
-        <input type="text" id="city" ref={cityInputRef} />
-        {!fromInputsValidity.city && <p>City name is not valid!</p>}
-      </div>
-      <div className={classes.actions}>
-        <button type="button" onClick={onCancel}>
-          Cancel
-        </button>
-        <button className={classes.submit}>Confirm</button>
-      </div>
-    </form>
+    <>
+      <h3>Please fill in the order details:</h3>
+      <form className={classes.form} onSubmit={confirmHandler}>
+        <div className={`${classes.control} ${fromInputsValidity.name ? '' : classes.invalid}`}>
+          <label htmlFor="name">Your Name</label>
+          <input type="text" id="name" ref={nameInputRef} />
+          {!fromInputsValidity.name && <p>Name is not valid!</p>}
+        </div>
+        <div className={`${classes.control} ${fromInputsValidity.street ? '' : classes.invalid}`}>
+          <label htmlFor="street">Street</label>
+          <input type="text" id="street" ref={streetInputRef} />
+          {!fromInputsValidity.street && <p>Street name is not valid!</p>}
+        </div>
+        <div className={`${classes.control} ${fromInputsValidity.postalCode ? '' : classes.invalid}`}>
+          <label htmlFor="postal">Postal Code</label>
+          <input type="text" id="postal" ref={postalInputRef} />
+          {!fromInputsValidity.postalCode && <p>Postal Code is not valid!</p>}
+        </div>
+        <div className={`${classes.control} ${fromInputsValidity.city ? '' : classes.invalid}`}>
+          <label htmlFor="city">City</label>
+          <input type="text" id="city" ref={cityInputRef} />
+          {!fromInputsValidity.city && <p>City name is not valid!</p>}
+        </div>
+        <div className={classes.actions}>
+          <button type="button" onClick={onCancel}>
+            Cancel
+          </button>
+          <button className={classes.submit}>Confirm</button>
+        </div>
+      </form>
+    </>
   );
 };
 
